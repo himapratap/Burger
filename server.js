415d1832c33270ecfa8144e35965c936360c4a0e
himapratap <himapratap@gmail.com>
2017-05-14 22:59:43 -0400

    heroku port change

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.set("view engine", "handlebars");
 
 app.use("/", routes);
 
-
-app.listen(port, function(){
-console.log("Listening on port :" ,port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
